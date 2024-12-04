c390e60259cc220955661d47cadb5af8a9c56ed0
Furkan <furkanamerikaa@gmail.com>
2024-12-04 11:59:29 -0500

    add vehicle and remove vehicle added to the InventoryDao class

--- src/main/java/com/yearup/dealership/db/InventoryDao.java
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -14,9 +14,27 @@ public class InventoryDao {
 
     public void addVehicleToInventory(String vin, int dealershipId) {
         // TODO: Implement the logic to add a vehicle to the inventory
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("INSERT INTO inventory (vin, dealership_id) VALUES (?, ?)")) {
+            statement.setString(1, vin);
+            statement.setInt(2, dealershipId);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
+
 
     public void removeVehicleFromInventory(String vin) {
         // TODO: Implement the logic to remove a vehicle from the inventory
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("DELETE FROM inventory WHERE vin = ?")) {
+            statement.setString(1, vin);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }

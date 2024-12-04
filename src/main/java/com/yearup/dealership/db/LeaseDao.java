b3ed35d812b044ea205b216819ba1b43094e113a
Furkan <furkanamerikaa@gmail.com>
2024-12-04 13:03:28 -0500

    Lease contract method added to the LeaseDao class

--- src/main/java/com/yearup/dealership/db/LeaseDao.java
+++ src/main/java/com/yearup/dealership/db/LeaseDao.java
@@ -16,5 +16,17 @@ public class LeaseDao {
 
     public void addLeaseContract(LeaseContract leaseContract) {
         // TODO: Implement the logic to add a lease contract
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement("INSERT INTO lease_contracts (vin, lease_start, lease_end, monthly_payment) VALUES (?, ?, ?, ?)")) {
+                statement.setString(1, leaseContract.getVin());
+                statement.setObject(2, leaseContract.getLeaseStart());
+                statement.setObject(3, leaseContract.getLeaseEnd());
+                statement.setDouble(4, leaseContract.getMonthlyPayment());
+                statement.executeUpdate();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
     }
-}
+
